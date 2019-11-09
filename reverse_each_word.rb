cf1cd23af9b2aae84e26fad5000f4522de43485c
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-09 09:26:50 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,29 @@
+def reverse_each_word (phrase)
+  
+  newPhrase = phrase.split
+  splitWords = []
+  
+  newPhrase.map do |words|
+    index = 0
+    while index < words.length do
+      splitWords << words[index].reverse
+      index += 1
+    end
+  end
+  
+  
+
+  
+  
+  # newPhrase = phrase.split
+  # reverseWords = []
+  # index = 0
+  # while index < newPhrase.size do
+  #   reverseWords << newPhrase[index].reverse
+  #   index += 1
+  # end
+  # newPhrase = reverseWords.join(" ")
+  # return newPhrase
+  
+  
+end
